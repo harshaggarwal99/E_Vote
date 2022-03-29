a9f0e7894dc968732273aedc1e75f55c7da1a0a7
Harsh <72658442+harshaggarwal99@users.noreply.github.com>
2022-03-29 14:57:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/evote/SignIn.java
@@ -0,0 +1,16 @@
+package com.example.evote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+    }
+}
